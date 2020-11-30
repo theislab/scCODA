8af44b8826c3d8e92e823db1370c2cb46d475e28
johannesostner <johannes.ostner@online.de>
2020-11-30 11:01:26 +0100

    readthedocs fix v11

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,8 @@ master_doc = 'index'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc',
+extensions = ['numpydoc',
+              'sphinx.ext.autodoc',
               'sphinx.ext.intersphinx',
               'sphinx.ext.doctest',
               'sphinx.ext.coverage',
