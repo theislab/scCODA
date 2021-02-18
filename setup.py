1f75082df0546fa6509abf1ae997ef4324b0d073
johannesostner <johannes.sotner@online.de>
2021-02-18 09:15:50 +0100

    PyPi hotfix

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/theislab/scCODA'
 EMAIL = 'johannes.ostner@helmholtz-muenchen.de'
 AUTHOR = 'Johannes Ostner, Benjamin Schubert'
 REQUIRES_PYTHON = '>=3.7.0'
-VERSION = "0.1.1-hotfix.1"
+VERSION = "0.1.1.post1"
 
 # What packages are required for this module to be executed?
 REQUIRED = [
